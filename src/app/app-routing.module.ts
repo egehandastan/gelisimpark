5df312ebf857ea454dbfe0e5bd0caadb0b6a126f
egehandastan <dstnegehan60@gmail.com>
2022-08-12 09:47:52 +0300

    committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [];
+import { TabGroupComponent } from './tab-group/tab-group.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+
+const routes: Routes = [
+ 
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
